0542e3797ea53925245f778d401afdf1b5042e06
Oliver Kuopus <141285426+oakuopus@users.noreply.github.com>
2024-04-17 15:07:03 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 $( function() {
-    $( ".item-header" ).accordion({
-        active: false
+    $( ".accordion-items" ).accordion({
+        collapsible: true,
+        active: false,
+        icons: false
+    });    
+    $(window).on("scroll", function(){
+        if($(window).scrollTop() == 0){
+            $("header").css({
+                "background-color": "initial"
+            });
+        }else{
+        $("header").css({
+            "background-color": "#1B1B1B",
+        });
+        }   
     });
-    
 });
