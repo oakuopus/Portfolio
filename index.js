2bce3741c8e4e9d01bd6b0a86a8a524dbcde14de
Oliver Kuopus <141285426+oakuopus@users.noreply.github.com>
2024-04-17 18:18:22 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 $( function() {
     $( ".accordion-items" ).accordion({
         collapsible: true,
-        active: false,
+        active: true,
         icons: false
-    });    
+    }); 
+    $('.about-info').css('height', '32vw');
+    $('.contact-info').css('height', '4vh');
+    $('.work-info').css('height', '20vw');
+    $(".item-header").on("click", function(){
+        $(this).toggleClass("dropped").siblings().removeClass("dropped");
+
+    });
     $(window).on("scroll", function(){
         if($(window).scrollTop() == 0){
             $("header").css({
@@ -12,6 +19,7 @@ $( function() {
         }else{
         $("header").css({
             "background-color": "#1B1B1B",
+            "opacity": "80%",
         });
         }   
     });
