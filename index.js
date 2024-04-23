0701875e1074fb98ad5c91d5ec2723d7ac26458a
Oliver Kuopus <141285426+oakuopus@users.noreply.github.com>
2024-04-22 20:35:38 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-$( function() {
-    $( ".accordion-items" ).accordion({
+$( function() { //ensures document is loaded before any jquery is ran.
+    $( ".accordion-items" ).accordion({ //method from the accordion library, simple options to alter it in my liking.
         collapsible: true,
         active: true,
         icons: false
     }); 
-    $('.about-info').css('height', '27vw');
+
+    $('.about-info').css('height', '27vw'); //.css used to override any css I did not want that was forced with the accordion library
     $('.contact-info').css('height', '8vw');
     $('.work-info').css('height', '34vw');
-    $(".item-header").on("click", function(){
-        $(this).toggleClass("dropped").siblings().removeClass("dropped");
 
+    $(".item-header").on("click", function(){ // function to keep opened accordions hover styles
+        $(this).toggleClass("dropped").siblings().removeClass("dropped");
     });
-    $(window).on("scroll", function(){
+
+    $(window).on("scroll", function(){ // function to create dynamic header, which only shows when scrollTop is not 0
         if($(window).scrollTop() == 0){
             $("header").css({
                 "background-color": "initial"
@@ -24,7 +26,7 @@ $( function() {
         }   
     });
 
-    const swiper = new Swiper('.swiper', {
+    const swiper = new Swiper('.swiper', { // The given code from swiper javascript library to easily create a carousel.
         direction: 'horizontal',
         loop: true,
         slides: 3,
